8198e6104a83384af99e2a8a7a068059233aa122
penta <martin@claesson.co>
2020-08-19 00:31:35 +0200

    hide mobile buttons aswell, Fix dynamic further up for this instead

--- web/resources/views/common/navbar.blade.php
+++ web/resources/views/common/navbar.blade.php
@@ -2,12 +2,14 @@
     <v-img src="/img/HD_logo.png" max-width="100"></v-img>
     <v-toolbar-title>
         <v-btn
+            :hidden="$vuetify.breakpoint.mobile"
             class="hidden-sm-and-down"
             text
             href="{{route('index')}}">
             <span style="color: #E0218A">H</span>-Sektionens Datorförening
         </v-btn>
         <v-btn
+            :hidden="$vuetify.breakpoint.mobile"
             class="hidden-md-and-up"
             text
             href="{{route('index')}}">
@@ -35,30 +37,32 @@
         <!--<v-btn icon href="https://www.facebook.com/HDChalmers/"><v-icon>mdi-instagram</v-icon></v-btn>-->
     </v-toolbar-items>
     <v-toolbar-items class="hidden-lg-and-up">
-        <v-btn icon x-large href="{{route('committee.index')}}">
+        @if(false)
+        <v-btn icon :x-large="!$vuetify.breakpoint.mobile" :small="$vuetify.breakpoint.mobile" href="{{route('committee.index')}}">
             <v-icon title="Sittande">mdi-account-group</v-icon>
         </v-btn>
-        <v-btn icon x-large href="{{route('event.index')}}">
+        <v-btn icon :x-large="!$vuetify.breakpoint.mobile" :small="$vuetify.breakpoint.mobile" href="{{route('event.index')}}">
             <v-icon title="Evenemang">mdi-calendar-star</v-icon>
         </v-btn>
-        <v-btn icon x-large href="{{route('products.index')}}">
+        <v-btn icon :x-large="!$vuetify.breakpoint.mobile" :small="$vuetify.breakpoint.mobile" href="{{route('products.index')}}">
             <v-icon title="Prislista">mdi-cash</v-icon>
         </v-btn>
         @if (\Illuminate\Support\Facades\Auth::check())
-            <v-btn icon x-large href="{{route('account.index')}}">
+            <v-btn icon :x-large="!$vuetify.breakpoint.mobile" :small="$vuetify.breakpoint.mobile" href="{{route('account.index')}}">
                 <v-icon title="Strecklista">mdi-format-list-checkbox</v-icon>
             </v-btn>
         @endif
-        <v-btn icon x-large href="{{route('games.index')}}">
+        <v-btn icon :x-large="!$vuetify.breakpoint.mobile" :small="$vuetify.breakpoint.mobile" href="{{route('games.index')}}">
             <v-icon title="Våra Spel">mdi-gamepad-variant</v-icon>
         </v-btn>
-        <v-btn icon x-large href="{{route('contact')}}">
+        @endif
+        <v-btn icon :x-large="!$vuetify.breakpoint.mobile" :small="$vuetify.breakpoint.mobile" href="{{route('contact')}}">
             <v-icon title="Kontakta Oss">mdi-phone</v-icon>
         </v-btn>
-        <v-btn icon x-large href="https://www.facebook.com/HsektionenChalmers/">
+        <v-btn icon :x-large="!$vuetify.breakpoint.mobile" href="https://www.facebook.com/HsektionenChalmers/">
             <span style="color: #E0218A">H</span>
         </v-btn>
-        <v-btn color="blue" icon x-large href="https://www.facebook.com/HDChalmers/">
+        <v-btn color="blue" icon :x-large="!$vuetify.breakpoint.mobile" :small="$vuetify.breakpoint.mobile" href="https://www.facebook.com/HDChalmers/">
             <v-icon>mdi-facebook</v-icon>
         </v-btn>
         <!--<v-btn icon href="https://www.facebook.com/HDChalmers/"><v-icon>mdi-instagram</v-icon></v-btn>-->
