1379488ba7aca8ff8949517e81af4c88c7ee89dd
penta <martin@claesson.co>
2020-08-20 00:19:58 +0200

    Re-do the commmittee member list

--- web/resources/views/committee/list.blade.php
+++ web/resources/views/committee/list.blade.php
@@ -12,53 +12,67 @@
             <v-card-text>
                 <v-row>
                     <v-col cols="12" class="text-center">
-                        <h1>HD {{($active_year->year)}}/{{$active_year->year+1}}</h1>
+                        <h1>HD {{$active_year->displayed_year}}</h1>
                     </v-col>
                 </v-row>
                 <v-row>
                     <v-col cols="12" class="text-center">
-                        <v-img src="{{$active_year->group_photo ? Storage::url($active_year->group_photo) : '/img/unknown_group.png'}}" class="mx-auto" max-height="400px" contain></v-img>
+                        <v-img lazy-src="/img/unknown_group.png" src="{{$active_year->group_photo ? Storage::url($active_year->group_photo) : '/img/unknown_group.png'}}" class="mx-auto" max-height="400px" contain></v-img>
                         {{$active_year->description}}
                     </v-col>
                 </v-row>
                 <v-row class="mb-3 text-center">
                     @foreach($active_year->committee_members as $member)
                         <v-col cols="12" md="6" lg="4" xl="3">
-                            <v-img src="{{$member->image ? Storage::url($member->image) : "/img/unknown_profile.png"}}" class="mx-auto" max-height="200px" contain></v-img>
-                            <v-row>
-                                <v-col cols="12" class="text-center">
-                                    <h3>{{$member->name}}</h3>
-                                    <div class="subtitle-1 font-weight-bold">{{ucfirst($member->role)}}</div>
-                                </v-col>
-                            </v-row>
-                            @if($member->quote)
-                                <v-row>
-                                    <v-col cols="12" class="text-center">
-                                        <span class="subtitle-2">"{{$member->quote}}"</span>
-                                    </v-col>
-                                </v-row>
-                            @endif
-                            <v-row>
-                                <v-col cols="12">
-                                    {{$member->description}}<br/><br/>
-                                    @if($member->favourite_game)
-                                        <v-row>
-                                            <v-col cols="12">
-                                            <span
-                                                class="font-weight-bold">Favorit Spel: </span>{{$member->favourite_game}}
-                                            </v-col>
-                                        </v-row>
-                                    @endif
-                                    @if($member->favourite_sugar)
-                                        <v-row>
-                                            <v-col cols="12">
-                                        <span
-                                            class="font-weight-bold">Favorit Socker: </span>{{$member->favourite_sugar}}
-                                            </v-col>
-                                        </v-row>
-                                    @endif
-                                </v-col>
-                            </v-row>
+                            <v-avatar size="200px" class="elevation-1 mb-2">
+                                <v-img lazy-src="/img/unknown_profile.png" src="{{$member->image ? Storage::url($member->image) : "/img/unknown_profile.png"}}" class="mx-auto" max-height="200px" contain></v-img>
+                            </v-avatar>
+                            <v-card elevation="1" rounded>
+                                <v-card-title class="justify-center">
+                                    {{$member->name}}
+                                </v-card-title>
+                                <v-card-subtitle class="text-center">
+                                    {{ucfirst($member->role)}}
+                                </v-card-subtitle>
+                                <v-card-text class="py-0">
+                                    <v-list class="py-0">
+                                        @if($member->quote)
+                                            <v-list-item class="mx-0 py-0 text-center" style="min-height: 20px">
+                                                <v-list-item-subtitle>
+                                                    "{{$member->quote}}"
+                                                </v-list-item-subtitle>
+                                            </v-list-item>
+                                        @endif
+                                        @if($member->description)
+                                            <v-list-item class="mx-0 py-0 text-center" style="min-height: 20px">
+                                                <v-list-item-subtitle>
+                                                    {{$member->description}}
+                                                </v-list-item-subtitle>
+                                            </v-list-item>
+                                        @endif
+                                        @if($member->favourite_game)
+                                            <v-list-item class="mx-0 py-0 text-left" style="min-height: 20px">
+                                                <v-list-item-title>
+                                                    Favoritspel
+                                                </v-list-item-title>
+                                                <v-list-item-subtitle>
+                                                    {{$member->favourite_game}}
+                                                </v-list-item-subtitle>
+                                            </v-list-item>
+                                        @endif
+                                        @if($member->favourite_game)
+                                            <v-list-item class="mx-0 py-0 text-left" style="min-height: 20px">
+                                                <v-list-item-title>
+                                                    Favoritsocker
+                                                </v-list-item-title>
+                                                <v-list-item-subtitle>
+                                                    {{$member->favourite_sugar}}
+                                                </v-list-item-subtitle>
+                                            </v-list-item>
+                                        @endif
+                                    </v-list>
+                                </v-card-text>
+                            </v-card>
                         </v-col>
                     @endforeach
                 </v-row>
