23fcb162841b29d589bbaafc4d929c5d2e8cc9f2
penta <martin@claesson.co>
2020-11-02 15:22:11 +0100

    Update Routes to newer format

--- web/routes/web.php
+++ web/routes/web.php
@@ -11,55 +11,72 @@
 |
 */
 
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\BoardGameGeekController;
+use App\Http\Controllers\CommitteeController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\CounterController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\GameController;
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\LoehkController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\StreckController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', "IndexController@index")->name('index');
+Route::get('/', [IndexController::class, 'index'])->name('index');
 
 Auth::routes();
-Route::resource('/committee', "CommitteeController");
-Route::resource('/event', 'EventController');
-Route::resource('/account', 'AccountController')->middleware('auth');
-Route::get('/products/download', 'ProductController@download');
-Route::resource('/products', 'ProductController')->except(['show']);
-Route::post('/games/save', 'GameController@save');
-Route::resource('/games', 'GameController');
-Route::get('/contact', 'ContactController@index')->name('contact');
+Route::resource('/committee', CommitteeController::class);
+Route::resource('/event', EventController::class);
+Route::resource('/account', AccountController::class)->middleware('auth');
+Route::get('/products/download', [ProductController::class, 'download']);
+Route::resource('/products', ProductController::class)->except(['show']);
+Route::post('/games/save', [GameController::class, 'save']);
+Route::resource('/games', GameController::class);
+Route::get('/contact', [ContactController::class, 'index'])->name('contact');
 
-Route::get('/door', 'IndexController@door')->name('door');
+Route::get('/door', [IndexController::class, 'door'])->name('door');
 Route::middleware('auth')->prefix('loehk')->name('loehk.')->group(function() {
-    Route::get('/', 'LoehkController@index')->name('index');
-    Route::get('/stats', 'LoehkController@stats')->name('stats');
-    Route::get('/active_year', 'LoehkController@active_year')->name('active_year');
-    Route::post('/active_year/new', 'LoehkController@newActiveYear')->name('new_active_year');
-    Route::post('/active_year/{active_year}/update', 'LoehkController@update')->name('update_active_year');
-    Route::post('/committee_member', 'LoehkController@newCommitteeMember')->name('new_committee_member');
-    Route::post('/committee_member/{committee_member}', 'LoehkController@updateCommitteeMember')->name('committee_member');
-    Route::delete('/committee_member/{committee_member}', 'LoehkController@deleteCommitteeMember')->name('delete_committee_member');
-    Route::post('/event/new', 'LoehkController@newEvent')->name('new_event');
-    Route::post('/event', 'LoehkController@getEvents')->name('events');
-    Route::delete('/event/{event}', 'LoehkController@deleteEvent')->name('delete_event');
-    Route::post('/event/{event}/update', 'LoehkController@updateEvent')->name('update_event');
-    Route::get('/prices', 'LoehkController@getPrices')->name('prices');
-    Route::post('/prices', 'ProductController@newProduct')->name('newProduct');
-    Route::patch('/prices/{product}', 'ProductController@updateProduct')->name('updateProduct');
-    Route::delete('/prices/{product}', "ProductController@deleteProduct");
-    Route::get('/streck/print', 'StreckController@print')->name('streckPrint');
-    Route::get('/accounts', 'StreckController@getAccounts');
-    Route::post('/accounts', 'StreckController@newAccount');
-    Route::patch('/accounts/{account}', 'StreckController@updateAccount');
-    Route::delete('/accounts/{account}', 'StreckController@deleteAccount');
+    Route::get('/', [LoehkController::class, 'index'])->name('index');
+    Route::get('/stats', [LoehkController::class, 'stats'])->name('stats');
+
+    Route::get('/active_year', [LoehkController::class, 'active_year'])->name('active_year');
+    Route::post('/active_year/new', [LoehkController::class, 'newActiveYear'])->name('new_active_year');
+    Route::post('/active_year/{active_year}/update', [LoehkController::class, 'update'])->name('update_active_year');
+
+    Route::post('/committee_member', [LoehkController::class, 'newCommitteeMember'])->name('new_committee_member');
+    Route::post('/committee_member/{committee_member}', [LoehkController::class, 'updateCommitteeMember'])->name('committee_member');
+    Route::delete('/committee_member/{committee_member}', [LoehkController::class, 'deleteCommitteeMember'])->name('delete_committee_member');
+
+    Route::post('/event/new', [LoehkController::class, 'newEvent'])->name('new_event');
+    Route::post('/event', [LoehkController::class, 'getEvents'])->name('events');
+    Route::delete('/event/{event}', [LoehkController::class, 'deleteEvent'])->name('delete_event');
+    Route::post('/event/{event}/update', [LoehkController::class, 'updateEvent'])->name('update_event');
+
+    Route::get('/prices', [LoehkController::class, 'getPrices'])->name('prices');
+    Route::post('/prices', [ProductController::class, 'newProduct'])->name('newProduct');
+    Route::patch('/prices/{product}', [ProductController::class, 'updateProduct'])->name('updateProduct');
+    Route::delete('/prices/{product}', [ProductController::class, 'deleteProduct']);
+
+    Route::get('/streck/print', [StreckController::class, 'print'])->name('streckPrint');
+
+    Route::get('/accounts', [StreckController::class, 'getAccounts']);
+    Route::post('/accounts', [StreckController::class, 'newAccount']);
+    Route::patch('/accounts/{account}', [StreckController::class, 'updateAccount']);
+    Route::delete('/accounts/{account}', [StreckController::class, 'deleteAccount']);
 });
 
-Route::get('/counter', 'CounterController@index');
-Route::post('/counter/increase', 'CounterController@increase');
-Route::post('/counter/decrease', 'CounterController@decrease');
-Route::post('/counter/get', 'CounterController@get');
-Route::post('/counter/reset', 'CounterController@reset');
+Route::get('/counter', [CounterController::class, 'index']);
+Route::post('/counter/increase', [CounterController::class, 'increase']);
+Route::post('/counter/decrease', [CounterController::class, 'decrease']);
+Route::post('/counter/get', [CounterController::class, 'get']);
+Route::post('/counter/reset', [CounterController::class, 'reset']);
 
 
-Route::get('/contact', 'ContactController@index')->name('contact');
+Route::get('/contact', [ContactController::class, 'index'])->name('contact');
 
 Route::name('bgg.')->prefix('bgg')->group(static function() {
-    Route::post('/search', 'BoardGameGeekController@search')->name('search');
-    Route::post('/fetch/{id}', 'BoardGameGeekController@fetch')->name('fetch');
+    Route::post('/search', [BoardGameGeekController::class, 'search'])->name('search');
+    Route::post('/fetch/{id}', [BoardGameGeekController::class, 'fetch'])->name('fetch');
 });
