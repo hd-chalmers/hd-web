b75ac60be5421803845f0111b1034efa191b105e
penta <martin@claesson.co>
2020-11-18 14:43:29 +0100

    Cleanup imports

--- web/app/Models/Barcode.php
+++ web/app/Models/Barcode.php
@@ -7,23 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * App\Models\Barcode
  *
- * @property int $id
- * @property int $product_id
- * @property string $barcode
+ * @property int                             $id
+ * @property int                             $product_id
+ * @property string                          $barcode
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
- * @property-read \App\Models\Product $product
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Barcode newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Barcode newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Barcode query()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Barcode whereBarcode($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Barcode whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Barcode whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Barcode whereProductId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Barcode whereUpdatedAt($value)
+ * @property-read \App\Models\Product        $product
+ * @method static Builder|Barcode newModelQuery()
+ * @method static Builder|Barcode newQuery()
+ * @method static Builder|Barcode query()
+ * @method static Builder|Barcode whereBarcode( $value )
+ * @method static Builder|Barcode whereCreatedAt( $value )
+ * @method static Builder|Barcode whereId( $value )
+ * @method static Builder|Barcode whereProductId( $value )
+ * @method static Builder|Barcode whereUpdatedAt( $value )
  * @mixin \Eloquent
- * @property string|null $variant_name
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Barcode whereVariantName($value)
+ * @property string|null                     $variant_name
+ * @method static Builder|Barcode whereVariantName( $value )
  */
 class Barcode extends Model
 {
