82e7b9b8b38c646e00444106c9a6607a30b6d78a
Shinz <alen.mukaca@gmail.com>
2021-07-08 00:26:28 +0200

    💄 Gave the navbar a new look. It now has similar margins as the base content and each button has new icon. The structure of the elements has also been updated.

--- web/src/components/common/navbar.vue
+++ web/src/components/common/navbar.vue
@@ -1,26 +1,40 @@
 <template>
 <v-app-bar app elevate-on-scroll clipped-left>
-    <v-img src="/img/HD_logo.png" max-width="100"></v-img>
+  <v-container style="display: flex; align-items: center;">
+    <v-img src="/img/HD_logo.png" id="hd-logo" max-width="100"></v-img>
   <router-link to="/">
     <v-toolbar-title>
         <v-btn
             :hidden="$vuetify.breakpoint.mobile"
             class="hidden-sm-and-down"
-            text>
+            text
+            :small="$vuetify.breakpoint.mobile"
+        >
             <span style="color: #E0218A">H</span>-Sektionens Datorförening
         </v-btn>
         <v-btn
             :hidden="$vuetify.breakpoint.mobile"
             class="hidden-md-and-up"
             text>
-            <span style="color: #E0218A">H</span><span style="color: black">D</span>
+            <span style="color: #E0218A">H</span>
+          <span>D</span>
         </v-btn>
     </v-toolbar-title>
   </router-link>
     <v-spacer></v-spacer>
-    <v-toolbar-items class="hidden-md-and-down">
-        <v-btn to="/committee" text>Sittande</v-btn>
-        <v-btn to="/events" text>Evenemang</v-btn>
+    <v-toolbar-items  :style="$vuetify.breakpoint.smAndDown ? 'height: 56px;' : 'height: 64px;'">
+        <v-btn to="/committee" text :icon="$vuetify.breakpoint.smAndDown"  :small="$vuetify.breakpoint.mobile && !$vuetify.breakpoint.xsOnly" :x-small="$vuetify.breakpoint.xsOnly">
+          <users-icon class="navbar-icon"></users-icon>
+          <span class="hidden-sm-and-down">
+            Sittande
+          </span>
+        </v-btn>
+        <v-btn to="/events" text :icon="$vuetify.breakpoint.smAndDown" :x-small="$vuetify.breakpoint.xs" :small="$vuetify.breakpoint.mobile && !$vuetify.breakpoint.xsOnly">
+          <calendar-icon class="navbar-icon"></calendar-icon>
+          <span class="hidden-sm-and-down">
+            Evenemang
+          </span>
+        </v-btn>
         <!--
         @if(false)
             <v-btn text href="products">Prislista</v-btn>
@@ -30,55 +44,25 @@
             <v-btn text href="games">Våra Spel</v-btn>
         @endif
         -->
-        <v-btn to="/contact" text>Kontakta Oss</v-btn>
-        <v-btn text href="https://www.facebook.com/HsektionenChalmers/"><span
-                style="color: #E0218A">H</span>-Sektionen
+        <v-btn to="/contact" text :icon="$vuetify.breakpoint.smAndDown" :x-small="$vuetify.breakpoint.xs" :small="$vuetify.breakpoint.mobile && !$vuetify.breakpoint.xsOnly">
+          <phone-call-icon class="navbar-icon"></phone-call-icon>
+          <span class="hidden-sm-and-down">
+            Kontakta Oss
+          </span>
         </v-btn>
-        <v-btn color="blue" icon href="https://www.facebook.com/HDChalmers/" :small="$vuetify.breakpoint.mobile">
-            <v-icon>mdi-facebook</v-icon>
+        <v-btn icon color="pink" href="https://www.facebook.com/HsektionenChalmers/" :x-large="!$vuetify.breakpoint.mobile" :x-small="$vuetify.breakpoint.xs" :small="$vuetify.breakpoint.mobile && !$vuetify.breakpoint.xsOnly">
+          <v-img contain id="h-tek-img" width="28px" height="28px" src="/img/H-sektionen.svg" />
+        </v-btn>
+        <v-btn color="blue" icon href="https://www.facebook.com/HDChalmers/" :x-large="!$vuetify.breakpoint.mobile" :x-small="$vuetify.breakpoint.xs" :small="$vuetify.breakpoint.mobile && !$vuetify.breakpoint.xsOnly">
+            <facebook-icon/>
         </v-btn>
 
-      <v-btn @click="$vuetify.theme.dark = !$vuetify.theme.dark" icon  :small="$vuetify.breakpoint.mobile">
-        <v-icon>icon-ic_fluent_dark_theme_24_regular</v-icon>
+      <v-btn @click="$vuetify.theme.dark = !$vuetify.theme.dark" icon :x-large="!$vuetify.breakpoint.mobile" :x-small="$vuetify.breakpoint.xs" :small="$vuetify.breakpoint.mobile && !$vuetify.breakpoint.xsOnly">
+        <sun-icon :hidden="$vuetify.theme.dark"/> <moon-icon :hidden="!$vuetify.theme.dark"/>
       </v-btn>
         <!--<v-btn icon href="https://www.facebook.com/HDChalmers/"><v-icon>mdi-instagram</v-icon></v-btn>-->
     </v-toolbar-items>
-    <v-toolbar-items class="hidden-lg-and-up">
-        <v-btn to="/committee" icon :x-large="!$vuetify.breakpoint.mobile" :small="$vuetify.breakpoint.mobile">
-            <v-icon title="Sittande">mdi-account-group</v-icon>
-        </v-btn>
-        <v-btn to="/events" icon :x-large="!$vuetify.breakpoint.mobile" :small="$vuetify.breakpoint.mobile">
-            <v-icon title="Evenemang">mdi-calendar-star</v-icon>
-        </v-btn>
-      <!--
-        @if(false)
-        <v-btn icon :x-large="!$vuetify.breakpoint.mobile" :small="$vuetify.breakpoint.mobile" href="products">
-            <v-icon title="Prislista">mdi-cash</v-icon>
-        </v-btn>
-        @if (\Illuminate\Support\Facades\Auth::check())
-            <v-btn icon :x-large="!$vuetify.breakpoint.mobile" :small="$vuetify.breakpoint.mobile" href="account">
-                <v-icon title="Strecklista">mdi-format-list-checkbox</v-icon>
-            </v-btn>
-        @endif
-        <v-btn icon :x-large="!$vuetify.breakpoint.mobile" :small="$vuetify.breakpoint.mobile" href="games">
-            <v-icon title="Våra Spel">mdi-gamepad-variant</v-icon>
-        </v-btn>
-        @endif
-        -->
-        <v-btn to="/contact" icon :x-large="!$vuetify.breakpoint.mobile" :small="$vuetify.breakpoint.mobile">
-            <v-icon title="Kontakta Oss">mdi-phone</v-icon>
-        </v-btn>
-        <v-btn icon :x-large="!$vuetify.breakpoint.mobile" href="https://www.facebook.com/HsektionenChalmers/">
-            <span style="color: #E0218A">H</span>
-        </v-btn>
-        <v-btn color="blue" icon :x-large="!$vuetify.breakpoint.mobile" :small="$vuetify.breakpoint.mobile" href="https://www.facebook.com/HDChalmers/">
-            <v-icon>mdi-facebook</v-icon>
-        </v-btn>
-      <v-btn @click="$vuetify.theme.dark = !$vuetify.theme.dark" icon  :small="$vuetify.breakpoint.mobile">
-        <v-icon>mdi-brightness-6</v-icon>
-      </v-btn>
-        <!--<v-btn icon href="https://www.facebook.com/HDChalmers/"><v-icon>mdi-instagram</v-icon></v-btn>-->
-    </v-toolbar-items>
+  </v-container>
 </v-app-bar>
 </template>
 
@@ -86,11 +70,76 @@
   .v-toolbar a{
   text-decoration: none;
   }
+
+  .v-btn:hover span #h-tek-img .v-image__image{
+    background-image: url("/img/H-sektionen-colored.svg") !important;
+  }
+  .v-btn span #h-tek-img .v-image__image{
+    transition: background-image 0.28s;
+  }
+
+  .v-toolbar .v-btn__content > svg {
+    width: 21px;
+    height: 21px;
+  }
+
+  .navbar-icon{
+    margin-right: 5px;
+  }
+
+  .v-app-bar .v-toolbar__content .v-btn.theme--light{
+    color: rgba(0, 0, 0, 0.87);;
+  }
+
+  .v-app-bar .v-toolbar__content .v-btn--active {
+    border-bottom: 3px solid #E0218A;
+    padding-top: 3px !important;
+  }
+
+  .v-app-bar .v-toolbar__content .v-btn--active::before {
+    opacity: 0;
+  }
+
+  @media only screen and (max-width: 960px) {
+    .v-app-bar .v-btn--icon{
+      margin: 0 5px;
+    }
+
+    .navbar-icon{
+      margin-right: 0;
+    }
+  }
+
+  @media only screen and (max-width: 600px) {
+     .v-toolbar .v-btn__content > svg {
+      width: 16px;
+      height: 16px;
+    }
+
+    #hd-logo {
+      max-width: 50px !important;
+    }
+
+    #h-tek-img{
+      width: 16px !important;
+      height: 16px !important;
+    }
+  }
 </style>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import {CalendarIcon, UsersIcon, PhoneCallIcon, SunIcon, FacebookIcon, MoonIcon} from 'vue-feather-icons'
 
-@Component
+@Component({
+  components: {
+    UsersIcon,
+    CalendarIcon,
+    PhoneCallIcon,
+    SunIcon,
+    FacebookIcon,
+    MoonIcon
+  }
+})
 export default class navbar extends Vue {}
 </script>
