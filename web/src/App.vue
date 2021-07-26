cf851a52873937b518e684628f7601800a65642d
Shinz <alen.mukaca@gmail.com>
2021-07-26 22:01:58 +0200

    🔥 Cleaned up an removed in app.vue since it was unused

--- web/src/App.vue
+++ web/src/App.vue
@@ -4,31 +4,7 @@
 
 <script lang="ts">
   import {Component, Vue} from 'vue-property-decorator'
-  import Base from '@/views/layouts/base.vue'
 
-  @Component({
-    components: {
-      Base
-    }
-  })
+  @Component
   export default class App extends Vue{}
 </script>
- <style>
- .slide-fade-enter-active {
-   transition: all .5s ease-out;
- }
- .slide-fade-leave-active {
-   transition: all .4s ease-in-out;
- }
- .slide-fade-enter{
-   transform: translateY(100vh);
-   opacity: 0;
- }
- .slide-fade-leave-to{
-   opacity: 0;
-   transform: translateY(-100vh);
-   height: 0;
-   padding: 0;
-   box-sizing: border-box;
- }
- </style>
