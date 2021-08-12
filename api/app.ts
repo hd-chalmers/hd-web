5c1a0072ae185ef5847ac157e6af64dd44bac2cc
Shinz <alen.mukaca@gmail.com>
2021-08-12 21:23:47 +0200

    🐛 Fixed a bug where cwebp binary cannot be run in container. Also added graceful shutdown

--- api/app.ts
+++ api/app.ts
@@ -10,6 +10,9 @@ import { LogStyle } from './src/logStyles'
 import morgan from 'morgan'
 import fs from 'fs'
 import AsyncRedisClient from './src/declarations/asyncRedisClient'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import webp from 'webp-converter'
 
 const asyncRedis = new AsyncRedisClient({
   host: process.env.REDIS_URL,
@@ -20,10 +23,11 @@ if(!fs.existsSync('./storage/logs')) {
   fs.mkdirSync('./storage/logs', {recursive: true})
   fs.writeFileSync('./storage/logs/httpRequests.log', '')
 }
+const httpLogStream = fs.createWriteStream('./storage/logs/httpRequests.log')
     app.use(morgan(
       'combined',
       {
-        stream: fs.createWriteStream('./storage/logs/httpRequests.log')
+        stream: httpLogStream
       }))
 app.use(morgan( LogStyle.bg.white + LogStyle.fg.black
   + ` :date[iso] | :method | :url ${LogStyle.reset} Status :status, :response-time ms, http :http-version & :req[content-type]`,
@@ -67,11 +71,47 @@ app.use((req, res, next) => {
 })
 
 app.get('/', (req, res) => res.send('Api is running'))
-app.listen(parseInt(<string>process.env.API_PORT), () => {
-  console.log(LogStyle.bg.green + LogStyle.fg.black + ' ' + new Date().toISOString() +
-    ` | Server ${LogStyle.reset} API Server is running at ` + process.env.API_FULL_URL)
-})
+webp.grant_permission()
 
 for (const route of routes){
   new route(app, db, sql, asyncRedis)
 }
+
+const server = app.listen(parseInt(<string>process.env.API_PORT), () => {
+  console.log(LogStyle.bg.green + LogStyle.fg.black + ' ' + new Date().toISOString() +
+    ` | Server ${LogStyle.reset} API Server is running at ` + process.env.API_FULL_URL)
+})
+
+process.on('SIGINT', () => {
+  console.info(LogStyle.bright + 'Terminating gracefully...' + LogStyle.reset)
+  server.close((err) => {
+    if(err) {
+      console.error(err)
+      process.exit(1)
+      return
+    }
+    httpLogStream.destroy()
+    console.info('HTTP server terminated...')
+
+    db.dispose().then(
+      () => {
+        console.log('database connection closed...')
+        asyncRedis.quit(err => {
+          if(err){
+            console.error(err)
+            process.exit(1)
+            return
+          }
+          console.info('redis connection closed...')
+          process.exit(0)
+          return
+        })
+      },
+      err => {
+        console.error(err)
+        process.exit(1)
+        return
+      }
+    )
+  })
+})
