c73470dcef384de1d4bc4218929c334ef4c0f14c
Shinz <alen.mukaca@gmail.com>
2021-08-12 21:19:43 +0200

    🗃️ Added triggers so updated_at columns updated themselves now

--- db/migrations/01-games-and-qol-changes.sql
+++ db/migrations/01-games-and-qol-changes.sql
@@ -61,3 +61,55 @@ alter table purchase_history_products alter column updated_at set default curren
 alter table telescope_entries alter column created_at set default current_timestamp;
 alter table users alter column created_at set default current_timestamp;
 alter table users alter column updated_at set default current_timestamp;
+
+-- Created triggers to automatically update the updated_at columns
+CREATE OR REPLACE FUNCTION trg_fn_updated_at()
+    RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = current_timestamp;
+    RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+CREATE TRIGGER trg_update_at_tbl_games BEFORE UPDATE
+    ON games FOR EACH ROW EXECUTE PROCEDURE
+    trg_fn_updated_at();
+CREATE TRIGGER trg_update_at_tbl_accounts BEFORE UPDATE
+    ON accounts FOR EACH ROW EXECUTE PROCEDURE
+    trg_fn_updated_at();
+CREATE TRIGGER trg_update_at_tbl_barcodes BEFORE UPDATE
+    ON barcodes FOR EACH ROW EXECUTE PROCEDURE
+    trg_fn_updated_at();
+CREATE TRIGGER trg_update_at_tbl_categories BEFORE UPDATE
+    ON categories FOR EACH ROW EXECUTE PROCEDURE
+    trg_fn_updated_at();
+CREATE TRIGGER trg_update_at_tbl_committee_members BEFORE UPDATE
+    ON committee_members FOR EACH ROW EXECUTE PROCEDURE
+    trg_fn_updated_at();
+CREATE TRIGGER trg_update_at_tbl_door_statuses BEFORE UPDATE
+    ON door_statuses FOR EACH ROW EXECUTE PROCEDURE
+    trg_fn_updated_at();
+CREATE TRIGGER trg_update_at_tbl_events BEFORE UPDATE
+    ON events FOR EACH ROW EXECUTE PROCEDURE
+    trg_fn_updated_at();
+CREATE TRIGGER trg_update_at_tbl_game_genre BEFORE UPDATE
+    ON game_genre FOR EACH ROW EXECUTE PROCEDURE
+    trg_fn_updated_at();
+CREATE TRIGGER trg_update_at_tbl_game_owners BEFORE UPDATE
+    ON game_owners FOR EACH ROW EXECUTE PROCEDURE
+    trg_fn_updated_at();
+CREATE TRIGGER trg_update_at_tbl_platforms BEFORE UPDATE
+    ON game_platforms FOR EACH ROW EXECUTE PROCEDURE
+    trg_fn_updated_at();
+CREATE TRIGGER trg_update_at_tbl_products BEFORE UPDATE
+    ON products FOR EACH ROW EXECUTE PROCEDURE
+    trg_fn_updated_at();
+CREATE TRIGGER trg_update_at_tbl_purchase_histories BEFORE UPDATE
+    ON purchase_histories FOR EACH ROW EXECUTE PROCEDURE
+    trg_fn_updated_at();
+CREATE TRIGGER trg_update_at_tbl_purchase_history_products BEFORE UPDATE
+    ON purchase_history_products FOR EACH ROW EXECUTE PROCEDURE
+    trg_fn_updated_at();
+CREATE TRIGGER trg_update_at_tbl_users BEFORE UPDATE
+    ON users FOR EACH ROW EXECUTE PROCEDURE
+    trg_fn_updated_at();
