591d69c48034be13b1283149341e4eccdaf0226c
Shinz <alen.mukaca@gmail.com>
2021-09-28 22:19:14 +0200

    🏷️ Added some more databse varaibles for accesing tables

--- api/src/database.ts
+++ api/src/database.ts
@@ -8,9 +8,9 @@ const db = createConnectionPool();
 export default db;
 
 // You can list whatever tables you actually have here:
-const {accounts, active_years, barcodes, categories, committee_members, events, game_owners, game_platforms, games,
+const {accounts, active_years, barcodes, categories, committee_members, events, game_owners, game_genre, game_platforms, games,
   products, purchase_histories, purchase_history_products, users} = tables<DatabaseSchema>({
   serializeValue,
 });
-export {accounts, active_years, barcodes, categories, committee_members, events, game_owners, game_platforms, games,
+export {accounts, active_years, barcodes, categories, committee_members, events, game_owners, game_genre, game_platforms, games,
         products, purchase_histories, purchase_history_products, users};
