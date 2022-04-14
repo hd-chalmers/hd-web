13b170b2f99115c2dbfc7d4ad7ec00bbbd4338ee
shinzenatt <42674003+ShinzenATT@users.noreply.github.com>
2022-04-14 16:30:51 +0200

    💄 Updated the navigation bar to be at bottom for mobile and use a navigation drawer

--- web/components/common/navbar.vue
+++ web/components/common/navbar.vue
@@ -1,80 +1,122 @@
 <template>
-<v-app-bar app elevate-on-scroll clipped-left>
-  <v-container style="display: flex; align-items: center;">
-    <v-img src="/img/HD_logo.webp" id="hd-logo" max-width="100" style="z-index: 10; cursor: pointer;" @click="$router.push('/')"></v-img>
-  <nuxt-link to="/">
-    <v-toolbar-title @click.once="setLoad(true, $event)">
-        <v-btn
-            class="hidden-sm-and-down"
-            text
-            :small="$vuetify.breakpoint.mobile"
-        >
-            <span style="color: #E0218A">H</span>-Sektionens Datorförening
-        </v-btn>
-        <v-btn
-            class="hidden-md-and-up"
-            text
-            :small="$vuetify.breakpoint.xs">
-            <span style="color: #E0218A">H</span>
-          <span>D</span>
-        </v-btn>
-    </v-toolbar-title>
-  </nuxt-link>
-    <v-spacer></v-spacer>
-    <v-toolbar-items :show-arrows="false" :style="$vuetify.breakpoint.smAndDown ? 'height: 56px;' : 'height: 64px;'">
-        <v-tabs optional id="navbarBtns" right>
-        <v-tab nuxt to="/committee" @click.once="setLoad(true, $event)">
-          <users-icon class="navbar-icon"></users-icon>
-          <span class="hidden-sm-and-down">
-            Sittande
-          </span>
-        </v-tab>
-        <v-tab nuxt to="/events" @click.once="setLoad(true, $event)">
-          <calendar-icon class="navbar-icon"></calendar-icon>
-          <span class="hidden-sm-and-down">
-            Evenemang
-          </span>
-        </v-tab>
-          <v-tab nuxt to="/games" @click.once="setLoad(true, $event)">
-            <layers-icon class="navbar-icon"/>
-            <span class="hidden-sm-and-down">
-            Spel
-          </span>
-          </v-tab>
-        <!--
-        @if(false)
-            <v-btn text href="products">Prislista</v-btn>
-            @if (\Illuminate\Support\Facades\Auth::check())
-                <v-btn text href="strecklista">Strecklista</v-btn>
+  <span class="d-print-none">
+    <v-app-bar app elevate-on-scroll clipped-left :bottom="$vuetify.breakpoint.xsOnly">
+      <v-container style="display: flex; align-items: center; padding: 0">
+        <v-img src="/img/HD_logo.webp" id="hd-logo" max-width="100" style="z-index: 10; cursor: pointer;" @click="$router.push('/')"></v-img>
+      <nuxt-link to="/">
+        <v-toolbar-title @click.once="setLoad(true, $event)">
+            <v-btn
+                class="hidden-sm-and-down"
+                text
+                :small="$vuetify.breakpoint.mobile"
+            >
+                <span style="color: #E0218A">H</span>-Sektionens Datorförening
+            </v-btn>
+            <v-btn
+                class="hidden-md-and-up"
+                text
+                :small="$vuetify.breakpoint.xs">
+                <span style="color: #E0218A">H</span>
+              <span>D</span>
+            </v-btn>
+        </v-toolbar-title>
+      </nuxt-link>
+        <v-spacer></v-spacer>
+        <v-toolbar-items :show-arrows="false" :style="$vuetify.breakpoint.smAndDown ? 'height: 56px;' : 'height: 64px;'">
+            <v-tabs optional id="navbarBtns" right>
+              <template v-if="$vuetify.breakpoint.smAndUp">
+                <v-tab nuxt to="/committee" @click.once="setLoad(true, $event)">
+                  <users-icon class="navbar-icon"></users-icon>
+                  <span class="hidden-sm-and-down">
+                    Sittande
+                  </span>
+                </v-tab>
+                <v-tab nuxt to="/events" @click.once="setLoad(true, $event)">
+                  <calendar-icon class="navbar-icon"></calendar-icon>
+                  <span class="hidden-sm-and-down">
+                    Evenemang
+                  </span>
+                </v-tab>
+                <v-tab nuxt to="/games" @click.once="setLoad(true, $event)">
+                  <disc-icon class="navbar-icon"/>
+                  <span class="hidden-sm-and-down">
+                  Spel
+                </span>
+                </v-tab>
+            <!--
+            @if(false)
+                <v-btn text href="products">Prislista</v-btn>
+                @if (\Illuminate\Support\Facades\Auth::check())
+                    <v-btn text href="strecklista">Strecklista</v-btn>
+                @endif
+                <v-btn text href="games">Våra Spel</v-btn>
             @endif
-            <v-btn text href="games">Våra Spel</v-btn>
-        @endif
-        -->
-          <v-tab style="color: #ea1d86;" target="_blank" href="https://www.facebook.com/HsektionenChalmers/" @click="$ga.event('Social', 'Htek link')">
-            <v-img contain id="h-tek-img" width="28px" height="28px" src="/img/H-sektionen.svg" />
-          </v-tab>
-          <v-btn style="background-color: transparent; height: 100%; border-radius: 0;" class="navBtn" depressed x-small @click="scrollToFooter()">
-            <phone-call-icon class="navbar-icon"></phone-call-icon>
-            <!--span class="hidden-sm-and-down">
-              Kontakta Oss
-            </span-->
-          </v-btn>
-          <!--v-tab style="color: #2196F3;" href="https://www.facebook.com/HDChalmers/">
-              <facebook-icon/>
-          </v-tab>
-          <v-tab style="color: #9C27B0;" icon href="https://www.instagram.com/hdchalmers/">
-              <instagram-icon/>
-          </v-tab-->
+            -->
+                <v-tab style="color: #ea1d86;" target="_blank" href="https://www.facebook.com/HsektionenChalmers/" @click="$ga.event('Social', 'Htek link')">
+                  <v-img contain id="h-tek-img" width="28px" height="28px" src="/img/H-sektionen.svg" />
+                </v-tab>
+              </template>
 
-        <v-btn style="background-color: transparent; height: 100%; border-radius: 0;" class="navBtn" depressed x-small @click="toggleTheme()">
-          <sun-icon :hidden="$vuetify.theme.dark"/> <moon-icon :hidden="!$vuetify.theme.dark"/>
-        </v-btn>
-        </v-tabs>
-        <!--<v-btn icon href="https://www.facebook.com/HDChalmers/"><v-icon>mdi-instagram</v-icon></v-btn>-->
-    </v-toolbar-items>
-  </v-container>
-  <v-progress-linear v-if="getLoad()" color="primary" fixed bottom indeterminate></v-progress-linear>
-</v-app-bar>
+              <v-btn style="background-color: transparent; height: 100%; border-radius: 0;" class="navBtn" depressed x-small @click="scrollToFooter()">
+                <phone-call-icon class="navbar-icon"></phone-call-icon>
+                <!--span class="hidden-sm-and-down">
+                  Kontakta Oss
+                </span-->
+              </v-btn>
+              <!--v-tab style="color: #2196F3;" href="https://www.facebook.com/HDChalmers/">
+                  <facebook-icon/>
+              </v-tab>
+              <v-tab style="color: #9C27B0;" icon href="https://www.instagram.com/hdchalmers/">
+                  <instagram-icon/>
+              </v-tab-->
+
+              <v-btn style="background-color: transparent; height: 100%; border-radius: 0;" class="navBtn" depressed x-small @click="toggleTheme()">
+                <sun-icon :hidden="$vuetify.theme.dark"/> <moon-icon :hidden="!$vuetify.theme.dark"/>
+              </v-btn>
+
+              <v-btn v-if="$vuetify.breakpoint.xsOnly" style="background-color: transparent; height: 100%; border-radius: 0;" depressed x-small  class="navBtn" @click="nav = !nav">
+                <menu-icon class="navbar-icon"/>
+              </v-btn>
+            </v-tabs>
+            <!--<v-btn icon href="https://www.facebook.com/HDChalmers/"><v-icon>mdi-instagram</v-icon></v-btn>-->
+        </v-toolbar-items>
+      </v-container>
+      <v-progress-linear v-if="getLoad()" color="primary" fixed bottom indeterminate></v-progress-linear>
+    </v-app-bar>
+
+    <v-navigation-drawer app clipped v-model="nav" v-if="$vuetify.breakpoint.xsOnly">
+      <v-list style="position: absolute; bottom: 0; width: 100%;" nav>
+        <v-list-item-group color="primary">
+          <v-list-item href="https://www.facebook.com/HsektionenChalmers/">
+            <v-list-item-icon> <v-img contain width="24" height="24" id="h-tek-img" src="/img/H-sektionen.svg" /> </v-list-item-icon>
+            <v-list-item-title style="color: #ea1d86"> H-sektionens Sida</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item to="/" nuxt>
+            <v-list-item-icon> <home-icon/> </v-list-item-icon>
+            <v-list-item-title> Startsidan </v-list-item-title>
+          </v-list-item>
+
+          <v-list-item to="/committee" nuxt>
+            <v-list-item-icon> <users-icon/> </v-list-item-icon>
+            <v-list-item-title> Sittande </v-list-item-title>
+          </v-list-item>
+
+          <v-list-item to="/events" nuxt>
+            <v-list-item-icon> <calendar-icon/> </v-list-item-icon>
+            <v-list-item-title> Evenemang </v-list-item-title>
+          </v-list-item>
+
+          <v-list-item to="/games" nuxt>
+            <v-list-item-icon> <disc-icon/> </v-list-item-icon>
+            <v-list-item-title> Spel </v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+
+      </v-list>
+
+    </v-navigation-drawer>
+  </span>
 </template>
 
 <style lang="scss">
@@ -83,10 +125,10 @@
   text-decoration: none;
   }
 
-  .v-tab:hover #h-tek-img .v-image__image{
+  .v-tab:hover #h-tek-img .v-image__image, .v-list-item:hover #h-tek-img .v-image__image{
     background-image: url("/img/H-sektionen-colored.svg") !important;
   }
-  .v-tab #h-tek-img .v-image__image{
+  .v-tab #h-tek-img .v-image__image, .v-list-item #h-tek-img .v-image__image{
     transition: background-image 0.28s;
   }
 
@@ -136,29 +178,16 @@
   }
 
   @media only screen and (max-width: 600px) {
-     .v-toolbar .v-tab > svg, .navBtn svg {
-      width: 16px;
-      height: 16px;
-    }
-
-    .v-app-bar .v-toolbar__content .v-tabs > .v-tabs-bar .v-tab, .v-tabs-bar__content > .navBtn.v-btn{
-      min-width: 21px !important;
-    }
 
     #hd-logo {
       max-width: 50px !important;
-    }
-
-    #h-tek-img{
-      width: 16px !important;
-      height: 16px !important;
     }
   }
 </style>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import {CalendarIcon, UsersIcon, PhoneCallIcon, SunIcon, MoonIcon, ShoppingCartIcon, LayersIcon } from 'vue-feather-icons'
+import {CalendarIcon, UsersIcon, PhoneCallIcon, SunIcon, MoonIcon, ShoppingCartIcon, DiscIcon, MenuIcon, HomeIcon } from 'vue-feather-icons'
 
 @Component({
   components: {
@@ -168,7 +197,9 @@ import {CalendarIcon, UsersIcon, PhoneCallIcon, SunIcon, MoonIcon, ShoppingCartI
     SunIcon,
     MoonIcon,
     ShoppingCartIcon,
-    LayersIcon
+    DiscIcon,
+    MenuIcon,
+    HomeIcon
   }
 })
 export default class navbar extends Vue {
@@ -184,6 +215,7 @@ export default class navbar extends Vue {
 
   loading = false
   lock = false
+  nav = false
 
   getLoad(): boolean{
     return this.loading
